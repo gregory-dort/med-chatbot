8eec8cf29c2c451dfdaa9011a1619113ecda1a09
Flebory <123499686+Flebory@users.noreply.github.com>
2024-11-24 14:25:28 -0500

    initializing SQLite Database

---
+++ server/create_database.py
@@ -0,0 +1,19 @@
+import sqlite3
+
+# Connect to SQLite database (Create if it doesn't exist)
+conn = sqlite3.connect('user_data.db')
+cursor = conn.cursor()
+
+#Create a table for storing user login data
+cursor.execute('''
+CREATE TABLE IF NOT EXISTS users (
+    username TEXT PRIMARY KEY NOT NULL UNIQUE,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP)
+''')
+
+# Commit changes and close the connection
+conn.commit()
+conn.close()
+
+print("Database and 'users' table created successfully.")
