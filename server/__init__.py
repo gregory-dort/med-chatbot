46a77ebedbfacd115e3937b5a5f5bcda8c48ac63
Flebory <123499686+Flebory@users.noreply.github.com>
2024-11-25 12:22:09 -0500

    Implemented Account Creation API

--- server/__init__.py
+++ server/__init__.py
@@ -6,7 +6,22 @@ from werkzeug.security import generate_password_hash, check_password_hash
 app = Flask(__name__)
 CORS(app)
 
+@app.route('/api/create-account', methods = 'POST')
+def create_account():
+    data = request.json
+    print("payload received", data)
 
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        return jsonify(
+            {
+            "message": f"Account for {username} created successfully.",
+            "received_payload": data
+            })
+    else:
+        return jsonify({"error": "Invalid input"}), 400    
 
 if __name__ == "__main__":
     app.run(debug = True, use_reloader = False)
