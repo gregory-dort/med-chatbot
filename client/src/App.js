ecf91648ffdb799b2a7a81abc8abc99db3811d5b
Flebory <123499686+Flebory@users.noreply.github.com>
2024-12-05 11:35:07 -0500

    Created Login and CreateAccount JSX Components

--- client/src/App.js
+++ client/src/App.js
@@ -1,56 +1,13 @@
-import React, { useState } from 'react'; 
+import React, { useState } from 'react';
+import Login from 'login.js';
+import CreateAccount from 'create-account.js'; 
 
 
 function App() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-  const [message, setMessage] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault(); 
-
-    const payload = { username, password }
-
-    console.log("payload being sent: ", payload);
-    // send our post request
-    fetch("http://127.0.0.1:5000/api/create-account", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify(payload),
-    })
-    .then((response) => response.json())
-    .then((data) => {
-      if (data.message) {
-        setMessage(data.message);
-      } else if (data.error) {
-        setMessage(data.error);
-      }
-    })
-    .catch((error) => console.error("Error:", error));
-  };
-
-
   return (
-    <div className="App">
-      <h1>Create Account</h1>
-      <form onSubmit={handleSubmit}>
-        <input 
-          type="text" 
-          value={username} 
-          onChange={(e) => setUsername(e.target.value)}
-          placeholder="Enter username"
-        />
-        <input 
-          type="password" 
-          value={password} 
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter password"
-        />
-        <button type="submit">Create Account</button> 
-      </form>
-      {message && <p>{message}</p>}
+    <div className = "App">
+      <Login />
+      <CreateAccount />
     </div>
   );
 }
